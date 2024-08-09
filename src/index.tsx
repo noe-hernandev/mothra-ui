f6f34ace33453e5231cea88fc3e5a3ffe6073de1
noeHernandez94 <noe@expresate.io>
2024-08-08 21:01:00 -0600

    update(index): Actualizacion de exportacion de index para preubas

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,3 @@
-import Tag from './components/Tag/Tag';
-import type { TagProps } from './utils/interfaces/Tag';
-// export function multiply(a: number, b: number): Promise<number> {
-//   return Promise.resolve(a * b);
-// }
-
-export {
-  Tag,
+export function multiply(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b);
 }
-
-export type{
-  TagProps,
-}
-
